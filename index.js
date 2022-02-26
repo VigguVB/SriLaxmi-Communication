e5ab03d425c30b55175f683970cdb5c0df7069e5
Vignesh Bhandarkar <vigneshbhandarkar2014@gmail.com>
2022-02-26 09:53:48 +0530

    version update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var date= new Date()
+var date = new Date()
 var freshDate = date.toDateString()
-function proceed(){
+function proceed() {
     let name = document.getElementById("name").value;
     let product = document.getElementById("product").value;
     let mobile = document.getElementById("mobNumber").value;
@@ -9,11 +9,8 @@ function proceed(){
     let charger = document.getElementById("charger").value
     let amount = document.getElementById("priceamount").value;
     let inWords = document.getElementById("inWords").value;
-    let invoiceNumber = localStorage.getItem('invoice')
-    console.log(invoiceNumber)
-    let newinvoice = Number(invoiceNumber)+1
-    document.getElementById("invoice").innerHTML = newinvoice;
-    localStorage.setItem('invoice', newinvoice);
+
+
     document.getElementById("cusname").innerHTML = name;
     document.getElementById("contact").innerHTML = mobile;
     document.getElementById("buydate").innerHTML = freshDate;
@@ -21,33 +18,47 @@ function proceed(){
     document.getElementById("battery1").innerHTML = battery;
     document.getElementById("charger1").innerHTML = charger;
     document.getElementById("item").innerHTML = product;
-   
-    let cgst = amount*0.09;
-    let sgst = amount*0.09;
-    document.getElementById("cgstcalc").innerHTML = cgst;
-    document.getElementById("sgstcalc").innerHTML = sgst;
+
+    let cgst = amount * 0.09;
+    let freshcgst = cgst.toFixed(2)
+    let sgst = amount * 0.09;
+    let freshsgst = sgst.toFixed(2);
+    document.getElementById("cgstcalc").innerHTML = freshcgst;
+    document.getElementById("sgstcalc").innerHTML = freshsgst;
     document.getElementById("gT").innerHTML = amount;
     document.getElementById("inwords1").innerHTML = inWords;
-    freshamount = amount-cgst-sgst;
-    document.getElementById("price").innerHTML = freshamount;
-console.log("hi")
-}
+    freshamount = amount - freshcgst - freshsgst;
+    freshAmountUpdate = freshamount.toFixed(2);
+    document.getElementById("price").innerHTML = freshAmountUpdate;
+    if (name != "" && product != "" && mobile != "" && imei != "" && battery != "" && charger != "" && amount > 0 && inWords != "") {
+        document.getElementById("proceed").style.display = 'none';
+        let invoiceNumber = localStorage.getItem('invoice')
 
-function submithandler(e){
-e.preventDefault();
-}
+        let newinvoice = Number(invoiceNumber) + 1;
 
-function printing(){
-    document.getElementsByClassName('inputs')[0].style.display='none';
-    document.getElementsByClassName('print')[0].style.display='none';
-console.log("vig")
-}
-
-function getInitialValue(){
-    
-    let value=  localStorage.getItem('invoice');
-    if(!value){
-        localStorage.setItem('invoice', 1060)
+        localStorage.setItem('invoice', newinvoice);
+        document.getElementById("invoice").innerHTML = newinvoice
+    } else {
+        alert("Please Enter All Details")
     }
-    document.getElementById("invoice").innerHTML= localStorage.getItem('invoice');
+
+}
+
+function submithandler(e) {
+    e.preventDefault();
+}
+
+function printing() {
+    document.getElementsByClassName('inputs')[0].style.display = 'none';
+    document.getElementsByClassName('print')[0].style.display = 'none';
+    console.log("vig")
+}
+
+function getInitialValue() {
+
+    let value = localStorage.getItem('invoice');
+    if (!value) {
+        localStorage.setItem('invoice', 0000)
+    }
+    document.getElementById("invoice").innerHTML = localStorage.getItem('invoice');
 }
